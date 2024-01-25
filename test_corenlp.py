b04569cf273531208e2f4000911e0da0b33c3375
Raghav-Sirigade <101243093+Raghav-Sirigade@users.noreply.github.com>
2024-01-25 06:33:03 +0000

    Added tests

--- test_corenlp.py
+++ test_corenlp.py
@@ -1,4 +1,13 @@
-from nlplogic.corenlp import search_wikipedia, summarize_wikipedia, get_text_blob, get_phrase
+from nlplogic.corenlp import search_wikipedia, summarize_wikipedia, get_phrase
+
 
 def test_get_phrase():
     assert 'leopardus' in get_phrase('ocelot')
+
+
+def test_search_wikipedia():
+    assert 'Ocelot' in search_wikipedia('ocelot')
+
+
+def test_summarize_wikipedia():
+    assert 'ocelot' in summarize_wikipedia('ocelot')
